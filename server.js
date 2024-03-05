ab78f75c75b7ae2e4b29a339fec00f1559d9833b
Tim <akezh@mail.ru>
2024-03-05 21:22:19 +0300

    master

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const pug = require('pug');
 const fs = require("fs");
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const auth = require('auth');
+
 
 function error_log(err) {
     let d = new Date();
@@ -14,6 +17,13 @@ function error_log(err) {
 const app = express();
 app.use('/media', express.static('media'));
 app.use(bodyParser.json());
+app.set('trust proxy', 1);
+app.use(session({
+    secret: 'j46eMKWwUFn3aWE252*Q24Y4XqKAGSGS',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }));
 
 app.get('/', (req, res) => {
     fs.readFile("books.json", (err, data) => {
@@ -39,7 +49,38 @@ app.get('/', (req, res) => {
 // добавление книги в корзину
 // @param {Request} req
 app.post('/buy', (req, res) => {
-    res.send(req.body);
+    // // 1. синхронная запись в файл:
+    // fs.writeFileSync("users_books.csv", "test");
+
+    // // 2. асинхронная запись в файл с фунцией обратного вызова:
+    // // fs.writeFile("users_books.csv", "test", function() {
+    // //     console.log("File has been writed");
+    // // });
+
+    // // 3. promise
+    // // const promise = writeFile("users_books.csv", "test");
+    // // promise.then(() => console.log("File has been writed"));
+    
+    // const userId = Math.ceil(Math.random() * 1000);
+    // const d = new Date();
+    // d.setHours(d.getHours() + 5);
+    // const expires = d.toUTCString();
+    // res.setHeader("Set-Cookie", "user=" + userId + "; expires=" + expires);
+    // res.send(req.body);
+  
+    req.session.book_id = req.body.book_id;
+    console.log(req.session);
+    res.sendStatus(200);
+});
+
+app.get('/auth', (req, res) => {
+    let filepath = path.join(__dirname, "views", "auth.pug");
+    let template = pug.renderFile(filepath, {page: "auth"});
+    res.send(template);
+});
+
+app.post('/auth', (req, res) => {
+    res.sendStatus(200);
 });
 
 app.get('/order', (req, res) => {
@@ -56,12 +97,11 @@ app.get('/profile', (req,res) => {
     res.send("Profile page will be soon added!");
 });
 
-
-
 app.get('/icons.css', (req, res) => {
     res.sendFile(path.join(__dirname, "node_modules/bootstrap-icons/font/bootsrap-icons.min.css"));
 });
 
+
 app.listen(3000, () => {
-    console.log(`Server started by address: http://localhost:3000`);
+    console.log(`Server started by address: http://bookshop.local:3000`);
 })
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/js/bootstrap";
 import "bootstrap/scss/bootstrap.scss";
 import "bootstrap-icons/font/bootstrap-icons.scss"
 
+
 let books = document.querySelectorAll('.card');
 let formBook = document.querySelector("#book-name");
 
@@ -20,3 +21,22 @@ for (let i = 0; i < books.length; i++) {
         }
     });
 }
+
+document.querySelector("#auth-submit")
+    .addEventListener('click', e => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://bookshop.local:3000/auth");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onloadend = function () {
+            if (xhr.status == 200) {
+                console.log(xhr.resposeText);
+            }
+        }
+        const login = document.querySelector("#username").value;
+        const password = document.querySelector("#password").value;
+        const body = JSON.stringify({
+            login, 
+            password
+        })
+        xhr.send(body);
+    });
